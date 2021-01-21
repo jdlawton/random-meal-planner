56b4653e2beb21a307c10c209c1d9266b92ed19c
jdlawton <joe.lawton@outlook.com>
2021-01-21 10:48:26 -0600

    Created arrays for possible meal choices

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log("Hello World");
+
+const breakfastArray = [
+    'bacon and eggs',
+    'toast',
+    'yogurt',
+    'pancakes',
+    'bagel',
+    'donut',
+    'breakfast burrito',
+    'fruit',
+    'cereal',
+    'oatmeal'
+];
+const lunchArray = [
+    'tuna sandwich',
+    'hamburger',
+    'gyro',
+    'tacos',
+    'soup',
+    'chicken salad',
+    'pb & j',
+    'salad',
+    'pasty',
+    'chili'
+];
+const dinnerArray = [
+    'pot roast',
+    'fried chicken',
+    'grilled salmon',
+    'roast chicken',
+    'pizza',
+    'roast beef',
+    'roasted brussel sprouts',
+    'pork chops in mushroom gravy',
+    'beef with broccoli',
+    'curry'
+];
+
